bc63528dc94fe9503e83d1e304b6094aded7c300
shinarasheed <rasheedshinaopeyemi@gmail.com>
2021-08-12 14:18:43 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,53 @@
+import dotenv from "dotenv";
+import express from "express";
+
+const app = express();
+const port = 3000;
+import Stripe from "stripe";
+
+dotenv.config();
+
+//Confirm the API version from your stripe dashboard
+const stripe = Stripe(process.env.SECRET_KEY, { apiVersion: "2020-08-27" });
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
+
+app.post("/create-payment-intent", async (req, res) => {
+  try {
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount: 1099, //lowest denomination of particular currency
+      currency: "usd",
+      payment_method_types: ["card"], //by default
+    });
+
+    const clientSecret = paymentIntent.client_secret;
+
+    res.json({
+      clientSecret: clientSecret,
+    });
+  } catch (e) {
+    console.log(e.message);
+    res.json({ error: e.message });
+  }
+});
+
+app.post("/payment-sheet", async (req, res) => {
+  // Use an existing Customer ID if this is a returning customer.
+  const customer = await stripe.customers.create();
+  const ephemeralKey = await stripe.ephemeralKeys.create(
+    { customer: customer.id },
+    { apiVersion: "2020-08-27" }
+  );
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: 1099,
+    currency: "usd",
+    customer: customer.id,
+  });
+  res.json({
+    paymentIntent: paymentIntent.client_secret,
+    ephemeralKey: ephemeralKey.secret,
+    customer: customer.id,
+  });
+});
