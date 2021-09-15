c803d62da6e5683d6d71f7e5626af9e051a4228b
shinarasheed <rasheedshinaopeyemi@gmail.com>
2021-09-15 13:19:31 +0100

    add braintree

--- index.js
+++ index.js
@@ -1,18 +1,27 @@
-import dotenv from "dotenv";
-import express from "express";
+const dotenv = require("dotenv");
+const express = require("express");
+const Stripe = require("stripe");
+const braintree = require("braintree");
 
 const app = express();
-
-import Stripe from "stripe";
-const port = 3000;
+const port = 5000;
 
 dotenv.config();
+
+const gateway = new braintree.BraintreeGateway({
+  environment: braintree.Environment.Sandbox,
+  merchantId: process.env.MERCHANT_ID,
+  publicKey: process.env.PUBLIC_KEY,
+  privateKey: process.env.PRIVATE_KEY,
+});
 
 const stripe = Stripe(process.env.SECRET_KEY, { apiVersion: "2020-08-27" });
 
 app.listen(port, () => {
   console.log(`app listening at ${port}`);
 });
+
+app.use(express.json());
 
 app.post("/create-payment-intent", async (req, res) => {
   try {
@@ -51,4 +60,40 @@ app.post("/payment-sheet", async (req, res) => {
     ephemeralKey: ephemeralKey.secret,
     customer: customer.id,
   });
+});
+
+// Braintree integration
+app.get("/api/generate/token", (req, res) => {
+  gateway.clientToken
+    .generate({
+      // customerId: aCustomerId,
+    })
+    .then((response) => {
+      // const clientToken = response.clientToken;
+      // res.status(200).send(clientToken);
+      res.status(200).send(response);
+    })
+    .catch((err) => {
+      res.status(500).send(err);
+    });
+});
+
+app.post("/api/checkout", (req, res) => {
+  const nonceFromTheClient = req.body.payment_method_nonce;
+  const { amount } = req.body;
+  gateway.transaction
+    .sale({
+      amount,
+      paymentMethodNonce: nonceFromTheClient,
+      // deviceData: deviceDataFromTheClient,
+      options: {
+        submitForSettlement: true,
+      },
+    })
+    .then((response) => {
+      res.status(200).send(response);
+    })
+    .catch((err) => {
+      res.status(500).send(err);
+    });
 });
